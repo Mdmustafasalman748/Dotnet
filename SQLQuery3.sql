178c2183462f066c9b7b963c2d7aec29b33c1325
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-13 20:39:12 -0400

    practice queries

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -85,3 +85,75 @@ order by sal desc
 
 select count(empid) as NOE, sum(sal) as SOS from tbl_emp
 where empgender='m'
+
+select top(1) * from tbl_emp
+where sal not in (select max(sal) from tbl_emp)
+order by sal desc
+
+select empname, sal from tbl_emp
+where sal = (select min(sal) from tbl_emp)
+
+select DateDiff(YY, 'hiredate' , Getdate()) as exp from tbl_emp 
+where empname='smith'
+
+
+select empname, DateDiff(YY, 'hiredate' , Getdate()) as exp from tbl_emp
+where empgender='f'
+
+select empname, empgender, 
+case when empgender='m' then 'MR'
+when empgender='f' then 'MRS' end as title from tbl_emp
+
+select depid, 
+case when depid is null then 0
+when depid is not null then depid
+end as did from tbl_emp
+
+select empname, 
+case when Datediff(YY, 'hiredate' , getdate()) < 5 then 'Beginner'
+when DATEDIFF(YY, 'hiredate', getdate()) between 5 and 10 then 'Intermedaite'
+when datediff(YY,'hiredate',getdate())>10 then 'Expert' end as eligibility from tbl_emp
+
+select empname, sal, depname from tbl_emp as E inner join tbl_dept as D on E.depid=D.depid
+
+select depname, dephod, sal, empname from tbl_dept as D inner join tbl_emp as E on D.depid=E.depid
+
+select empname, sum(sal) as SOS, depname from tbl_emp as E inner join tbl_dept as D on E.depid=D.depid
+where empgender='m'
+group by empname, depname
+having sum(sal) > 50000
+order by SOS desc
+
+--Normalization is the process of breaking the table into multiple tables
+
+-- It will compile the code once and will execute the code 'n' number of times
+
+create proc as getdeptdetail
+as
+select * from tbl_dept
+
+execute sp_getdeptdetail
+
+create proc sp_insertdeptdetails
+as
+insert into tbl_dept
+values('TEst1','Smith')
+
+exec sp_insertdeptdetails
+
+create proc sp_insertdeptdetails-v
+@depname varchar(100), 
+@dephod varchar(100)
+as 
+insert into tbl_dept
+values(@depname, @dephod)
+
+exec sp_insertdeptdetails-v 'test','smith'
+
+create view department as select depname, dephod from tbl_dept where depid=102
+
+
+
+
+
+
