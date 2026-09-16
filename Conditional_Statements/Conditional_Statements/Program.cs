3eff3ec003e7b2947d30f6665cb21db4f55c6f17
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-16 19:17:10 -0400

    if example

---
+++ Conditional_Statements/Conditional_Statements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 30;
+            if (a > b)
+            {
+                Console.WriteLine("A is Greater than B");
+            }
+            else
+            {
+                Console.WriteLine("B is Greater than A");
+            }
+            Console.ReadLine();
+        }
+    }
+}
