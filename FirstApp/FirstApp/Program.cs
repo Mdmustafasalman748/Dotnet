23d3478412edac8a9ed51b04cecfdf2ed550c4a1
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-16 19:12:38 -0400

    C# basics

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* CONTROL STRUCTURES*/
+            
+            /* CONDITIONAL STATEMENTS
+             1. if - for one or two conditions
+            syntax: if (condition){
+            statement;
+            }
+            2. else if - for more than two conditions
+            syntax: else if(condition){
+            statement;
+            }
+            3. switch - for options
+            syntax:switch(variable name){
+            case 1:
+                statement;
+            case 2:
+                statement;
+            default:
+                statement;
+            }
+            */
+
+            /*UNCONDITIONAL STATEMENTS
+             1. goto - to go back to a condition or a statement
+            2. break - to break the statement and come out of the loop
+            3. continue - to continue the iteration in the loop
+
+            Looping
+            1. for - used for finite iterations
+            syntax: for(initialization, condition/statement, increment/decrement){
+            statement;
+            }
+            2. while - used for infinite iterations
+            syntax: while(condition){
+            }
+            3. foreach - used for collections
+            foreach( Datatype in item objectname)
+            */
+        }
+    }
+}
