06051673f2ce54ec83cb9369bb051eeaf0428d08
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-15 21:31:47 -0400

    queries solved

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -6,3 +6,5 @@ where empgender='f'
 order by sal desc
 
 select depname, dephod, empname, sal from tbl_dept as D inner join tbl_emp as E on D.depid=E.empid
+
+select * from tbl_emp where sal between 50000 and 70000 and empname like '%A%'
