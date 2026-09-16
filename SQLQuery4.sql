244fc650a86ca110a530265cfb2ba4daef9d1182
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-15 21:34:20 -0400

    ending sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -8,3 +8,5 @@ order by sal desc
 select depname, dephod, empname, sal from tbl_dept as D inner join tbl_emp as E on D.depid=E.empid
 
 select * from tbl_emp where sal between 50000 and 70000 and empname like '%A%'
+
+--Sql is structured query language and it is not case sensitive. It is used as a database for the backend. RDBMS uses the concept of tables, tables contains rows(fields) and columns(attributes) 
