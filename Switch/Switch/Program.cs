7f0b326fc0083971c7066b07da283214f82f086b
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-16 19:42:01 -0400

    switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter then Pin");
+            int Pin = int.Parse(Console.ReadLine());
+            if (Pin == 1234)
+            {
+            l: Console.WriteLine("1.Withdrawl 2.Deposit 3.MiniStatement 4.Exit");
+                int ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("You have selected Withdrawl");
+                        break;
+                    case 2:
+                        Console.WriteLine("You have selected Deposit");
+                        break;
+                    case 3:
+                        Console.WriteLine("You have selected ministatement");
+                        break;
+                    case 4:
+                        Console.WriteLine("You have selected the exit");
+                        break;
+                    default:
+                        Console.WriteLine("Please select the correct option");
+                        goto l;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please! enter the correct pin");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
