6add55544265952881fd95c154b6f14d4cff9612
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-17 14:03:04 -0400

    return value example

---
+++ Return_value/Return_value/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_value
+{
+    public class Add
+    {
+        public int Display()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            return c;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add a = new Add();
+            int res=a.Display();
+            Console.WriteLine(res);
+            Console.ReadLine(); 
+        }
+    }
+}
