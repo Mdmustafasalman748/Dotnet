55ba69cd8aa07c5b2fbfcb77ffdd24eeffaaf2a6
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 12:12:43 -0400

    Namespace

---
+++ Namespace/Namespace/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace
+{
+    /*Namespace is a collection of classes and namespaces.
+     * A namespace can have multiple classes.*/
+    class Testing
+    {
+
+    }
+    class Test1
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test.test1cl obj = new Test.test1cl();
+            Test.Dotnet.dt objdt = new Test.Dotnet.dt();
+            obj.Display();
+            objdt.Display();
+            Console.ReadLine();
+        }
+    }
+}
+namespace Test
+{
+    namespace Dotnet
+    {
+        class dt
+        {
+            public void Display()
+            {
+                Console.WriteLine("This is Display1 Method of Dotnet Namespace");
+            }
+        }
+    }
+    class test1cl
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is Display Method of Test Namespace");
+        }
+    }
+}
