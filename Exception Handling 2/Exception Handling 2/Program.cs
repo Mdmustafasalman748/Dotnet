8ac3dea635f49eac9750b0eb3c933995853e5bcd
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 11:46:53 -0400

    Exception Handling 2

---
+++ Exception Handling 2/Exception Handling 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the number:");
+                int n = int.Parse(Console.ReadLine());
+                int res = 10 / n;
+                Console.WriteLine(res);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                    }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
