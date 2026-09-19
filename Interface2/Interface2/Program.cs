73c7c7e94dbc097353d24caaf3aa4475061f8fc8
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 13:03:54 -0400

    Interface2

---
+++ Interface2/Interface2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface2
+{
+    interface IStudent
+    {
+        void Marks(int M1,int M2);
+    }
+    public class NewStudent
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is a New Student class");
+        }
+    }
+    public class NewNewStudent : NewStudent, IStudent
+    {
+        public void Show()
+        {
+            Console.WriteLine("This is NewNewStudent Class");
+        }
+        public void Marks(int M1,int M2)
+        {
+            int M3 = M1 + M2;
+            Console.WriteLine("Your total is:"+M3);
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewNewStudent obj= new NewNewStudent();
+            obj.Display();
+            obj.Show();
+            obj.Marks(50, 70);
+            Console.ReadLine();
+            
+        }
+    }
+}
