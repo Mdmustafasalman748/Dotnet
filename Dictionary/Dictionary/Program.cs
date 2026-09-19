2756752a0473f85736545f519b04446180a9cc3a
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 11:53:26 -0400

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dictionary<keytype,valuetype> DictionaryName=new Dictionary<keytype,valuetype>();*/
+             Dictionary<String,int> age=new Dictionary<string,int>();
+            age.Add("John", 23);
+            age.Add("James", 27);
+            age.Add("Alex", 38);
+            age.Remove("John");
+            foreach (var item in age)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
