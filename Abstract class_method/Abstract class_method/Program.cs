5896a59caa75a1f94b3cfc01ea9e68a20ffbccfe
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 10:19:12 -0400

    Abstract Class Method

---
+++ Abstract class_method/Abstract class_method/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_method
+{
+    /*Abstract Class
+     * A class which is declared with a keyword abstract is known as abstract class.
+     * Abstract class contains abstract as well as non abstract members.
+     * We cannot create object of an abstract class but we can create reference of an abstract class.
+     * 
+     * Abstract Method
+     * A method which is declared with a keyword abstract is known as abstract method.
+     * Abstract method doesn't contain any body.
+     * If we declared abstract method in the base class then we have to override it in the derived class.
+     * If a method is abstract then the class should be abstract.
+     */
+
+    public abstract class Employee
+    {
+        public abstract double CalSal(double SPD, double NOD);
+    }
+    public class NewEmployee : Employee
+    {
+        public override double CalSal(double SPD, double NOD)
+        {
+            double Salary = SPD * NOD;
+            return Salary;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewEmployee obj= new NewEmployee();
+            double Salary = obj.CalSal(1500,22);
+            Console.WriteLine(Salary);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Abstract class_method/Abstract class_method/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Abstract class_method")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Abstract class_method")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("354477c5-c9d9-4eb2-816b-0a9a76c50ea4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
