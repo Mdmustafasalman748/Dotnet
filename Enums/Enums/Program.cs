3fba60fa6e0a7df021b5c7170bf24d4b6775ae0f
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-19 13:09:36 -0400

    Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    /*Enums 
+     * Enums represent a set of possible values.
+     * They are useful for switch construction and are extended to include 
+     * additional values as needed.
+     */
+    enum ChequeBookStyles
+    {
+        standard,
+        picture,
+        braille
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
