b46e7ef08e58fe27b69e04b8d145b428070397cd
Mustafa Salman <Mdmustafasalman748@gmail.com>
2026-09-24 21:24:24 -0400

    Updated code

--- Webpage/Admin/MasterPage.Master.cs
+++ Webpage/Admin/MasterPage.Master.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Webpage.Admin
 {
-    public partial class MasterPage : System.Web.UI.Page
+    public partial class MasterPage : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
